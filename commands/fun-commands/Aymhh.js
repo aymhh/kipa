f10a64a5761e0abfba75745a051d95c4eef807ac
itsaymhh <amr.alshmmaa@gmail.com>
2020-11-09 00:32:52 +1100

    fun commands additon

---
+++ commands/fun-commands/Aymhh.js
@@ -0,0 +1,8 @@
+module.exports.run = async (bot, message) => {
+        message.delete()
+        message.channel.send("Aymhh gay <a:cherryyes:724163868712239164>")
+    }
+    module.exports.help = {
+        name: "Aymhh",
+        description: "custom command made by Tazza#0001-(265468010658136064) on 11/8/2020 @ 11:49:25 PM"
+    }
---
+++ commands/fun-commands/Tazza.js
@@ -0,0 +1,8 @@
+module.exports.run = async (bot, message) => {
+        message.delete()
+        message.channel.send("Tazza is best <:ProudTaz:774977605383356416>")
+    }
+    module.exports.help = {
+        name: "Tazza",
+        description: "custom command made by Tazza#0001-(265468010658136064) on 11/8/2020 @ 11:45:20 PM"
+    }
