1923f53a40875c426e01939ee3c124ccb4a92c5e
itsaymhh <amr.alshmmaa@gmail.com>
2020-11-12 20:22:23 +1100

    removed idiot commands

--- commands/fun-commands/0.js
+++
@@ -1,7 +0,0 @@
-module.exports.run = async (bot, message) => {
-        message.delete()
-    }
-    module.exports.help = {
-        name: "0",
-        description: "custom command made by Orange#6266-(565295162389954607) on 11/12/2020 @ 8:9:56 AM"
-    }
--- commands/fun-commands/1.js
+++
@@ -1,7 +0,0 @@
-module.exports.run = async (bot, message) => {
-        message.delete()
-    }
-    module.exports.help = {
-        name: "1",
-        description: "custom command made by Orange#6266-(565295162389954607) on 11/12/2020 @ 8:26:26 AM"
-    }
