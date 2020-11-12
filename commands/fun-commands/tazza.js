ecf19302a6bfa7fb128627ce2db443a198c5b9b9
itsaymhh <68047389+aymhh@users.noreply.github.com>
2020-11-12 12:30:03 +1100

    custom commands

---
+++ commands/fun-commands/tazza.js
@@ -0,0 +1,8 @@
+module.exports.run = async (bot, message) => {
+        message.delete()
+        message.channel.send("Tazza is the best <:ProudTaz:774977605383356416>")
+    }
+    module.exports.help = {
+        name: "tazza",
+        description: "custom command made by Tazza#0001-(265468010658136064) on 11/12/2020 @ 12:29:5 PM"
+    }
