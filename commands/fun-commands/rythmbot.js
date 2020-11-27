c5d5098ce268a81dd0032ee68f8997b9229277ac
itsaymhh <amr.alshmmaa@gmail.com>
2020-11-28 01:36:08 +1100

    final push (no music as of yet)

---
+++ commands/fun-commands/rythmbot.js
@@ -0,0 +1,8 @@
+module.exports.run = async (bot, message) => {
+        message.delete()
+        message.channel.send("Rythm bot is a bitch")
+    }
+    module.exports.help = {
+        name: "rythmbot",
+        description: "custom command made by aymhh#1729-(176610715686273024) on 11/28/2020 @ 1:33:4 AM"
+    }
