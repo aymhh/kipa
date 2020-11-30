ae7127b774f2a852d6ee1ce0a70cdb118bcb2b97
itsaymhh <amr.alshmmaa@gmail.com>
2020-11-30 20:15:27 +1100

    Update protocolExit.js

--- commands/protocolExit.js
+++ commands/protocolExit.js
@@ -19,7 +19,7 @@ module.exports.run = async (bot, message, error) => {
     }
 
     message.channel.send("restarting...").then(message => {
-        return process.exit()
+         process.exit()
     })
     
     
