583d648dc8ac1810ae573b17aab3353e8024c344
itsaymhh <amr.alshmmaa@gmail.com>
2021-01-29 00:33:41 +1100

    Create welcomegif.js

---
+++ commands/welcomegif.js
@@ -0,0 +1,12 @@
+
+var welcomelinks = [
+    "https://vrscout.com/wp-content/uploads/2019/03/BearsVRpromo.png",
+    "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/55660762-c858-4a1b-9239-4e143f216fa4/ddb7pv0-b16feb5c-f16c-4a53-a883-3240632e712f.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvNTU2NjA3NjItYzg1OC00YTFiLTkyMzktNGUxNDNmMjE2ZmE0XC9kZGI3cHYwLWIxNmZlYjVjLWYxNmMtNGE1My1hODgzLTMyNDA2MzJlNzEyZi5naWYifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.DTVsyeKyk790j4l47OqXwUbjXSo6iXMBk4w6Aakx5YM",
+    "https://i.kym-cdn.com/photos/images/newsfeed/001/105/063/a95.gif",
+    "https://i.pinimg.com/originals/f7/7b/32/f77b32dddd0bf3ee83cae787e3455601.gif",
+    "https://i.imgur.com/oI34oST.gif",
+    "https://media1.tenor.com/images/79d8ebb1dd7dd9ef2367195d53177675/tenor.gif?itemid=14737417",
+    "https://thumbs.gfycat.com/BasicWarpedIraniangroundjay-size_restricted.gif"
+]
+
+var welcomeLinkSingle = welcomelinks[Math.floor(Math.random() * welcomelinks.length)]
