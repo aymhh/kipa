26e5dcd7a80495c82622c8aaf99901a7517a0c44
itsaymhh <amr.alshmmaa@gmail.com>
2021-01-31 04:53:37 +1100

    general cleanup

--- commands/sinfo.js
+++ commands/sinfo.js
@@ -5,25 +5,23 @@ module.exports.run = async (bot, message, args) => {
    // Restricts commands to bot commands channels
    let botCommandsChannel = message.guild.channels.cache.find(channel => channel.name === `${commands}`)
 
-   const wrongChannelEmbed = new Discord.MessageEmbed()
-    .setColor('#FF6961')
-    .setTitle("error!")
-    .setDescription("wrong channel!")
-    .addField("i live in:", `<#${botCommandsChannel.id}>`)
-    .setTimestamp()
-    .setFooter(message.author.tag + " | " + bot.user.username, message.author.displayAvatarURL({dynamic: true, size: 1024}))
-   if(message.channel != botCommandsChannel && message.author.id != message.guild.owner.id) {
-    message.delete()
-    return message.channel.send(wrongChannelEmbed).then(msg => msg.delete({timeout: 7000}));
-   } else {
-    let region = message.guild.region
-    const nitroBoosterRankSize = message.guild.roles.cache.find(r => r.name === "Server Booster").members.size
-    const serverembed = new Discord.MessageEmbed()
-      .setTitle(message.guild.name)
+    const wrongChannelEmbed = new Discord.MessageEmbed()
+      .setColor('#FF6961')
+      .setTitle("error!")
+      .setDescription("wrong channel!")
+      .addField("i live in:", `<#${botCommandsChannel.id}>`)
       .setTimestamp()
       .setFooter(message.author.tag + " | " + bot.user.username, message.author.displayAvatarURL({dynamic: true, size: 1024}))
-      .setColor(color)
-      .setThumbnail(message.guild.iconURL({dynamic: true, size: 1024}))
+    ;
+    if(message.channel != botCommandsChannel && message.author.id != message.guild.owner.id) {
+      message.delete()
+      return message.channel.send(wrongChannelEmbed).then(msg => msg.delete({timeout: 7000}));
+   } else {
+
+    let region = message.guild.region
+
+    const serverembed = new Discord.MessageEmbed()
+      .setTitle(message.guild.name)
       .addField("Owner", `<@${message.guild.owner.user.id}>`, true)
       .addField("Region", region.charAt(0).toUpperCase() + region.slice(1), true)
       .addField("Channels", message.guild.channels.cache.size, true)
@@ -35,14 +33,18 @@ module.exports.run = async (bot, message, args) => {
       .addField("Animated Emotes", message.guild.emojis.cache.filter(emoji => emoji.animated).size, true)
       .addField("Server Boost Level", message.guild.premiumTier, true)
       .addField("Server Boosts", message.guild.premiumSubscriptionCount, true)
-      .addField("Server Boosters", nitroBoosterRankSize, true)
+      .addField("Server Boosters", message.guild.roles.cache.find(r => r.name === "Server Booster").members.size, true)
       .addField("Creation Date", message.guild.createdAt)
       .addField("Server Splash Image:", "<a:giyuuWhistle:781512339031195678>")
       .setImage(message.guild.splashURL({size: 4096}))
+      .setTimestamp()
+      .setColor(color)
+      .setThumbnail(message.guild.iconURL({dynamic: true, size: 1024}))
+      .setFooter(message.author.tag + " | " + bot.user.username, message.author.displayAvatarURL({dynamic: true, size: 1024}))
     ;
-     return message.channel.send(serverembed)
+    return message.channel.send(serverembed)
   }
-}
+};
  
 module.exports.help = {
   name: "sinfo"
